daff0394dca2c0ebfd57724c18f6a910a2801906
RR-DS <robson.rodrigues@hotmail.com>
2021-03-18 18:57:52 -0300

    Initial commit

---
+++ app/src/main/java/com/appUFPR/Notification.java
@@ -0,0 +1,15 @@
+package com.appUFPR;
+
+public class Notification extends Message {
+
+private String fromName;
+
+public String getFromName () {
+	return fromName;
+}
+
+public void setFromName (String fromName) {
+	this.fromName = fromName;
+}
+
+}
